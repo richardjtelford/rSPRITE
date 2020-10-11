9b46db51cd3dc2ecd09adc0678312a274fd9972e
richardjtelford <richard.telford@bio.uib.no>
2020-10-11 22:11:25 +0200

    faceted plot rather than multiple plots

--- R/rSprite.buildCharts.R
+++ R/rSprite.buildCharts.R
@@ -1,23 +1,26 @@
-#' @importFrom gridExtra grid.arrange
 #' @importFrom moments skewness
+#' @importFrom dplyr %>% mutate group_by n
+#' @importFrom tidyr pivot_longer
+#' @importFrom forcats fct_reorder
+#' @importFrom ggplot2 ggplot aes geom_bar labs facet_wrap
+#' @importFrom rlang .data
 
 # Build a grid containing all the results charts.
 rSprite.buildCharts <- function (sample, scaleMin, scaleMax, gridSize) {
   rows <- sample$rows
   
-  nCases <- nrow(rows)
-  s <- paste("Plotting ", nCases, " unique solution", (if (nCases == 1) "" else "s"), "...", sep="")
-  rSprite.message(s, showNow=TRUE)
-  
-  if (nCases > 1) {
-    rows <- rows[order(apply(rows, 1, skewness)),]
-  }
-  
-  xMax <- max(rows)
-  yMax <- max(unlist(apply(rows, 1, table)))
-  grobs <- apply(rows, 1, function (x) {
-    rSprite.buildOneChart(x, scaleMin, scaleMax, gridSize, xMax, yMax, sample$label)
-  })
-  layoutMatrix <- matrix(1:(gridSize ^ 2), nrow=gridSize, ncol=gridSize, byrow=TRUE)
-  grid.arrange(grobs=grobs, layout_matrix=layoutMatrix)
+  df <- rows %>% as.data.frame() %>% 
+    mutate(ID = 1:n()) %>% 
+    pivot_longer(-.data$ID) %>% 
+    group_by(.data$ID) %>% 
+    mutate(sk = skewness(.data$value),
+           ID = factor(.data$ID), 
+           ID = fct_reorder(.data$ID, .data$sk))
+
+  plot <- ggplot(df, aes(x = .data$value)) +
+    geom_bar(fill="#0099ff", width=0.9) +
+    labs(title = sample$label, x = "response", y = "count") +
+    facet_wrap(~ .data$ID)
+  print(plot)
+
 }
